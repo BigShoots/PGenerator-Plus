f9b773c699f4911194f298103f8ea4a4f1637db6
Jordan <jordan@example.com>
2026-02-27 13:03:16 -0600

    Remove PgenCompanion

--- PgenCompanion/PGenerator Connect + Wake TV.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title PGenerator+ (Wake TV)
-echo.
-echo   Starting PGenerator+ with TV Wake...
-echo.
-powershell -ExecutionPolicy Bypass -File "%~dp0Connect-PGenerator.ps1" -WakeTV
-if errorlevel 1 pause
